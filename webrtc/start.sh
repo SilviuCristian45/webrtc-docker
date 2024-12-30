446697c672fb9f04b39e7c1b00baa7dca9b5df58
silviu <silviu.dinca@s20.ro>
2024-12-30 14:46:52 +0200

    initial

---
+++ webrtc/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Start the PeerJS server with CORS enabled for all origins
+npx peerjs --port 3001 --cors "https://192.168.88.168:3000" &
+
+# Start the Express app (or your app's start command)
+npm run start:dev
